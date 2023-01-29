b5d810c1bad8980e2934572ed0b2c2ccb275bd80
kitou88C39 <kanfutrooper@icloud.com>
2023-01-29 16:08:36 +0900

    memos_table.php再編集

--- database/migrations/2023_01_29_064549_create_memos_table.php
+++ database/migrations/2023_01_29_064549_create_memos_table.php
@@ -13,8 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('memos', function (Blueprint $table) {
-            $table->bigInteger('tag_id')->nullable()->after('user_id');
+        Schema::create('memos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->longText('content');
+            $table->integer('user_id');
+            $table->integer('status')->defalut('1');
+            // timestampと書いてしまうと、レコード挿入時、更新時に値が入らないので、DB::rawで直接書いてます
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
         });
     }
 
@@ -25,8 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('memos', function (Blueprint $table) {
-            $table->dropColumn('tag_id');
-        });
+        Schema::dropIfExists('memos');
     }
-}
+};
